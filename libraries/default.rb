35828ccbc7b931bfea34ebf17b20e53fdb930f73
Jeremy Hanmer <jeremy@fishbiscuits.org>
2015-10-20 16:59:42 -0700

    allow per-mon public nets

--- libraries/default.rb
+++ libraries/default.rb
@@ -114,7 +114,7 @@ def mon_addresses
       mon_ips = mons.map { |node| Chef::Recipe::Barclamp::Inventory.get_network_by_type(node, 'admin').address }
     else
       if node['ceph']['config']['global'] && node['ceph']['config']['global']['public network']
-        mon_ips = mons.map { |nodeish| find_node_ip_in_network(node['ceph']['config']['global']['public network'], nodeish) }
+        mon_ips = mons.map { |nodeish| find_node_ip_in_network(nodeish['ceph']['config']['global']['public network'], nodeish) }
       else
         mon_ips = mons.map { |node| node['ipaddress'] + ':6789' }
       end
