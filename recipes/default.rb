4b75378b74a4ab07ec600315b5275086f66de723
David Wahlstrom <david.wahlstrom@dreamhost.com>
2017-02-15 12:10:22 -0800

    Install the packages early on
    Parts of the conf recipe reference the "ceph" user, which comes from the
    package.  Therefore, the packages need to be installed before running
    the conf recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,10 @@
 include_recipe 'ceph::repo' if node['ceph']['install_repo']
-include_recipe 'ceph::conf'
 
 # Tools needed by cookbook
 node['ceph']['packages'].each do |pck|
   package pck
 end
+
+include_recipe 'ceph::conf'
 
 chef_gem 'netaddr'
