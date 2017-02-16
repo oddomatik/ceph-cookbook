facf57455f17714c7e52c85860aca051ec35d7fb
Jeremy Hanmer <jeremy@fishbiscuits.org>
2017-02-16 10:55:27 -0800

    switch back to using the ceph user by name

--- recipes/conf.rb
+++ recipes/conf.rb
@@ -8,8 +8,8 @@ unless node['ceph']['config']['fsid']
 end
 
 directory '/etc/ceph' do
-  owner '64045'
-  group '64045'
+  owner 'ceph'
+  group 'ceph'
   mode '0755'
   action :create
 end
@@ -22,7 +22,7 @@ template '/etc/ceph/ceph.conf' do
       :is_rgw => node['ceph']['is_radosgw']
     }
   }
-  owner '64045'
-  group '64045'
+  owner 'ceph'
+  group 'ceph'
   mode '0644'
 end
