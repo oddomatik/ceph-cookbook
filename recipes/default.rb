de87205b10c597becc7498df2d555d2613004c61
Jeremy Hanmer <jeremy@fishbiscuits.org>
2017-02-16 10:46:07 -0800

    honor package_action in ceph::default

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,9 @@ include_recipe 'ceph::repo' if node['ceph']['install_repo']
 
 # Tools needed by cookbook
 node['ceph']['packages'].each do |pck|
-  package pck
+  package pck do
+    action node['ceph']['package_action']
+  end
 end
 
 include_recipe 'ceph::conf'
